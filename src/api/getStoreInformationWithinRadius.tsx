a1356ee4d5e96c2e95ef1cdd6bedbe6a41100194
GreatDeveloper66 <ashaffe@gmail.com>
2026-03-29 21:16:42 -0500

    update getStoreInformation method with places type

--- src/api/getStoreInformationWithinRadius.tsx
+++ src/api/getStoreInformationWithinRadius.tsx
@@ -1,23 +1,23 @@
 //make an API call to the grocery service to get the grocery stores within a certain radius of the user's location
 import axios from 'axios';
 import dotenv from 'dotenv';
+import { places } from '../data_types/data_types';
 
 dotenv.config();
 
-const getStoreInformationWithinRadius = async (latitude: number, longitude: number, radius: number) => {
+const getStoreInformationWithinRadius = async (latitude: number, longitude: number, radius: number): Promise<places[]> => {
   try {
-    const response = await axios.get(process.env.GROCERY_SERVICES_URL!, {
+    const places = await axios.get<places[]>(process.env.GROCERY_SERVICES_URL!, {
         params: {
             latitude,
             longitude,
             radius
         }
     });
-    return response.data;
+    return places.data;
   } catch (error) {
     console.error('Error fetching store information:', error);
     throw error;
-  }
+  };
 };
-
 export default getStoreInformationWithinRadius;
