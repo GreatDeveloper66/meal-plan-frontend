98711a9eb79b0728b02466e5945891608edf3e6f
GreatDeveloper66 <ashaffe@gmail.com>
2026-04-05 16:55:05 -0500

    Update environment variable imports in GroceryServices

--- src/services/GroceryServices.tsx
+++ src/services/GroceryServices.tsx
@@ -2,7 +2,7 @@ import { locationAndRadiusFormState } from "../data_types/data_types";
 
 // import grocery services url and create urls for get grocery stores
 const API_BASE_URL =  process.env.REACT_APP_GROCERY_SERVICES_URL
-const GET_GROCERY_STORES_URL = `${API_BASE_URL}/find-grocery-markets-within-radius`;
+const GET_GROCERY_STORES_URL = `${API_BASE_URL}find-grocery-markets-within-radius`;
 
 // Function to get grocery stores within a radius
 export async function getGroceryStores(locationData: locationAndRadiusFormState): Promise<any> {
