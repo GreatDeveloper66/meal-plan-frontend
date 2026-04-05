c114aff3a41b551a84faf62b334eeee07ba45f79
GreatDeveloper66 <ashaffe@gmail.com>
2026-04-05 16:51:48 -0500

    Remove dotenv from getStoreInformationWithinRadius

--- src/api/getStoreInformationWithinRadius.tsx
+++ src/api/getStoreInformationWithinRadius.tsx
@@ -1,13 +1,11 @@
 //make an API call to the grocery service to get the grocery stores within a certain radius of the user's location
 import axios from 'axios';
-import dotenv from 'dotenv';
 import { places } from '../data_types/data_types';
 
-dotenv.config();
 
 const getStoreInformationWithinRadius = async (latitude: number, longitude: number, radius: number): Promise<places[]> => {
   try {
-    const places = await axios.get<places[]>(process.env.GROCERY_SERVICES_URL!, {
+    const places = await axios.get<places[]>(process.env.REACT_APP_GROCERY_SERVICES_URL!, {
         params: {
             latitude,
             longitude,
